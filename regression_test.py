2f09288a9a4acd56fe0786deaa7179c939fbcbbc
epernod <erik.pernod@gmail.com>
2018-11-15 11:52:09 +0100

    [Regression_test] Update script to take set sofa root path

--- regression_test.py
+++ regression_test.py
@@ -2,15 +2,19 @@ import os
 import sys
 
 #example of use:
-#python regression_test.py /home/epernod/projects/regression_test_sofa/build/bin/Regression_test ~/projects/sofa-src ~/projects/regression_test_sofa/src/references
+#python regression_test.py /home/epernod/projects/regression_test_sofa/build/bin/Regression_test ~/projects/sofa-src ~/projects/regression_test_sofa/src/references ~/projects/sofa-build/
 
 #arguments:
 # 0 - script name
 # 1 - regression_test binary path
 # 2 - regression scenes dir
 # 3 - regression references dir
+# 4 - sofa build dir
 
 os.environ["REGRESSION_SCENES_DIR"] = sys.argv[2]
 os.environ["REGRESSION_REFERENCES_DIR"] = sys.argv[3]
+
+os.environ["SOFA_ROOT"] = sys.argv[4]
+os.environ["SOFA_PLUGIN_PATH"] = sys.argv[4] + '/lib'
 
 os.system(sys.argv[1])
