07dcaed68cf8ca75ecc2d97d4001bfbfef880228
epernod <erik.pernod@gmail.com>
2024-08-02 15:36:36 +0200

    update period

--- Regression_test/Regression_test.cpp
+++ Regression_test/Regression_test.cpp
@@ -100,7 +100,7 @@ void StateRegression_test::runTestImpl(RegressionSceneData data, simulation::Nod
             times.push_back(root->getDt() * (data.m_steps - 1));
             writeVisitor.setExportTimes(times);
         }
-        //writeVisitor.setPeriod(1.0);
+        writeVisitor.setPeriod(1.0);
         writeVisitor.execute(root.get());
 
         sofa::component::playback::WriteStateActivator v_write(sofa::core::ExecParams::defaultInstance() /* PARAMS FIRST */, true);
